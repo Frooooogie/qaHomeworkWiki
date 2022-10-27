62097df52be206b5fc8acf5a06e510c2c1068a65
Morgan C <116771762+Frooooogie@users.noreply.github.com>
2022-10-26 22:05:07 -0400

    Update newFunction.ts

--- 2.2/newFunction.ts
+++ 2.2/newFunction.ts
@@ -1,11 +1,16 @@
 export function myFunction(myNum: number): string {
     if (myNum == 5) {
         return 'true'
-    } else if() {
-
-    }else if (){
-
-    }else {
+    } else if(myNum < 5 && myNum > 0) {
+        return 'false'
+    }else if (myNum > 5){
+        return 'big'
+    }else (myNum);{
+        return 'negative'
         
     }
 }
+console.log(myFunction(5));
+console.log(myFunction(3));
+console.log(myFunction(6));
+console.log(myFunction(-2));
